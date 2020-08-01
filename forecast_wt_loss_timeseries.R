874c1669f267e0a4d8affbd82b62ebe6c1caef66
Amit Arora <amit.arora@hughes.com>
2020-08-01 02:57:33 +0000

    add forecasting, lots of code cleanup and reorg

---
+++ forecast_wt_loss_timeseries.R
@@ -0,0 +1,57 @@
+
+#' forecast_wt_loss_timeseries
+#' Function uses the prophet library to make a forecast. It uses the built in dygraph library to create a
+#' chart object. An annotation is added on the chart object to indicate the date when the target weight loss
+#' is achieved.
+#' NOTE: As with any other forecast, it needs to be understood that this is valid only if the conditions
+#' that exist in the training data continue in the future as well i.e. if there is no intervention then
+#' this forecast should hold.
+forecast_wt_loss_timeseries <- function(df_tidy, pname, pname_initial, weight_cap, weight_floor, forecast_range_in_days, weight_target) {
+  # get the weight loss timeseries for this person and 
+  # rename the fields as prophet requires 
+  flog.info(glue("forecast_wt_loss_timeseries,
+                 pname={pname},
+                 weight_cap={weight_cap},
+                 weight_floor={weight_floor},
+                 forecast_range_in_days={forecast_range_in_days},
+                 weight_target={weight_target}"), name=LOGGER)
+  
+  df_wt <- df_tidy %>%
+    filter(metric=="Weight" & name==pname) %>%
+    rename(ds=Date, y=value) %>%
+    select(ds, y)
+  flog.info(glue("forecast_wt_loss_timeseries, shape of the df_wt dataframe is {nrow(df_wt)}x{ncol(df_wt)}"), name=LOGGER)
+  
+  # set the cap and floor because we want to get to a saturated
+  # minimum i.e. obviously the weight loss cannot continue infinitely.
+  df_wt$cap <- as.integer(weight_cap)
+  df_wt$floor <- as.integer(weight_floor)
+  
+  # for the saturating minimum we use the logistic model
+  m <- prophet(df_wt, growth = 'logistic')
+  
+  # create a dataframe for the desired future duration
+  future <- make_future_dataframe(m, periods = as.integer(forecast_range_in_days))
+  
+  # same floor and cap for the future as well
+  future$floor <-  as.integer(weight_floor)
+  future$cap <- as.integer(weight_cap)
+  
+  # run the forecast
+  forecast <- predict(m, future)
+
+  # find the date on which the target will be achieved. This is done by keeping only the entries
+  # where the yhat is <= target and then finding the first such entry
+  target_achieved_date <- forecast %>%
+    filter(yhat <= as.integer(weight_target)) %>%
+    arrange(desc(yhat)) %>%
+    head(1) %>%
+    mutate(ds = as.character(ds)) %>%
+    pull(ds)
+  
+  # all done, create an interactive plot object using dygraph. Use the first letter of the name as the
+  # annotation symbol
+  dyplot.prophet(m, forecast) %>%
+    dyAnnotation(target_achieved_date, text = pname_initial, tooltip = glue("Target: {weight_target} lb, Expected date: {target_achieved_date}"))
+  
+}
---
+++ globals.R
@@ -0,0 +1,54 @@
+# global constants
+# most of these should be configuration parameters
+
+APP_NAME <- "biometric_tracker"
+LOG_FILE <- file.path("/tmp", glue("{APP_NAME}.log"))
+LOGGER <- APP_NAME
+
+START_DATE <- "2020-02-17"
+DATA_DIR <- "data"
+
+# person 1 and 2 details
+P1_NAME <- "Nidhi"
+P1_NAME_INITIAL <- "N"
+P2_NAME <- "Amit"
+P2_NAME_INITIAL <- "A"
+
+P1_TARGET_WEIGHT <- 128
+P1_WEIGHT_CAP <- 160
+P1_WEIGHT_FLOOR <- 120
+
+P2_TARGET_WEIGHT <- 190
+P2_WEIGHT_CAP <- 260
+P2_WEIGHT_FLOOR <- 180
+
+P1_DATA_FPATH <- file.path(DATA_DIR, glue("{P1_NAME}.csv"))
+P2_DATA_FPATH <- file.path(DATA_DIR, glue("{P2_NAME}.csv"))
+
+CAPTION <- "Source: Daily measurements done @home"
+MONTH_ABB <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+# file containing dates and details for important events that could impact this analysis
+IMPORTANT_DATES_FNAME <- "important_dates.csv"
+IMPORTANT_DATES_FPATH <- file.path(DATA_DIR, IMPORTANT_DATES_FNAME)
+
+# charting related, how far should be the annoation from a point on the graph
+NUDGE_X <- 1
+NUDGE_Y <- 5
+CHART_ELEMENT_TEXT_SIZE <- 20
+
+MAIN_PAGE_CHART_TITLE <- "Journey to health"
+N_FOR_LAST_N_POUNDS_OF_INTREST <- 10
+
+# gauge
+SUCCESS_RANGE <- c(80, 100)
+WARNING_RANGE <- c(40, 79)
+DANGER_RANGE <- c(0, 39)
+
+# slider inputs
+FORECAST_DURATIONS <- c(60, 90, 180, 365)
+SELECTED_FORECAST_DURATION <- 180
+FRACTION_FOR_MIN_WEIGHT <- 0.7
+WEIGHT_SLIDER_STEP_SIZE <- 1
+
+
